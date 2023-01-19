4365be8429afceae7acc0277cdaa6d74d976ae30
praveen-raj-143 <praveenrajgreath@gmail.com>
2023-01-19 19:45:59 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    Name1 : "",
+    Department : "",
+    Rating : "",
+    person : []
+  };
+   changecontent =(e) =>{
+    // console.log("abcd")
+    this.setState({[e.target.name] : e.target.value});
+  }
+
+  submitcontent = (e) => {
+    const personObj = {
+Name : this.state.Name1,
+Department : this.state.Department,
+Rating : this.state.Rating
+    }
+    const arr = this.state.person;
+    arr.push(personObj);
+    this.setState({person : arr})
+  }
+  render() {
+    return (
+      <div >
+       <div className="pos">
+       <h1 className="header">EMPLOYEE FEEDBACK FORM</h1>
+       <form >
+        <label className="text" htmlFor="Name">Name : </label>
+        <input type="text" name="Name1" className="box" value={this.state.Name} onChange={this.changecontent} ></input>
+        <br/>
+        <br/>
+        <label className="text">Department : </label>
+        <input type="text" name="Department" className="box" value={this.state.Department}  onChange={this.changecontent} ></input>
+        <br/>
+        <br/>
+        <label className="text">Rating : </label>
+        <input type="Number" name="Rating" className="box" value={this.state.Rating}  onChange={this.changecontent} ></input>
+        <br/>
+        <br/>
+        <button className="text" type="button" onClick={this.submitcontent}>Submit</button>
+       </form>
+       </div>
+<br/>
+<br/>
+<br/>
+       <div className="containersubmit">
+        {this.state.person.map((value, index) => {
+          return(
+           
+              <div key={index} className="contentsubmit">Name : {value.Name} | Department : {value.Department} | Rating : {value.Rating}</div>
+            
+          )
+        })}
+       </div>
+      </div>
+    ); 
+  }
 }
 
 export default App;
